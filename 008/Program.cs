c73958f5e4363036d2271a3ad51ff024fc7820eb
Boris Shapovalov <bashap@bk.ru>
2022-04-21 01:35:44 +0300

    add 008

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет,является ли этот день выходным.
+
+
+int Number = new Random().Next(1, 8);
+string[] Days = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+string Result = "";
+
+if (Number > 5) Result = ($".holiday");
+else Result = ($", workday");
+
+Console.WriteLine($"{Number} number day, its  {Days[Number - 1]}{Result}");
