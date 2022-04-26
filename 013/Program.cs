65634eb6e6751436f7b414dbc028e43a163782fd
Boris Shapovalov <bashap@bk.ru>
2022-04-26 17:05:54 +0300

    add 013

---
+++ 013/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int b = 0;
+
+while (a > 0)
+{
+    b = a % 10;
+    result = result + b;
+    a = a / 10;
+}
+
+Console.WriteLine($"Сумма цифр равна {result}");
