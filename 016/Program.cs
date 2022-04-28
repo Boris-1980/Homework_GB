e1d8582e53c241c2d95b7d83af6d7f946fc31b46
Boris Shapovalov <bashap@bk.ru>
2022-04-29 00:50:54 +0300

    Add 016

---
+++ 016/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void Random(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(-100, 100);
+    }
+}
+int SumNegative(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0) result += array[i];
+    }
+    return result;
+}
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int[] number = new int[4];
+Random(number);
+Print(number);
+Console.WriteLine($"Cумма элементов на нечетных позициях равна = {SumNegative(number)}");
