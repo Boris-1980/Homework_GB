d8ae430f4546764e37988193202fb40338353df8
Boris Shapovalov <bashap@bk.ru>
2022-05-18 20:43:57 +0300

    Задача 50: Написать программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет

---
+++ 021/Program.cs
@@ -0,0 +1,53 @@
+﻿// Написать  программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 50);
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите колиство строк в массиве ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+string Position(int[,] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (num == array[m, n]) result += $"({m}, {n})";
+        }
+    }
+    if (result == string.Empty) 
+    result = "Такого элемента нет";
+
+
+    return result;
+}
+Console.WriteLine(Position(array));
